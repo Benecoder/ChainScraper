c7f48ac68115e8b695f6b9ccb1c8f45b6b014d27
ChillerDragon <ChillerDragon@gmail.com>
2018-10-05 23:13:31 +0200

    Check if the key is in the database

--- sql.py
+++ sql.py
@@ -39,8 +39,20 @@ def PrintPubKeys():
     row = c.fetchall()
     print(str(row))
 
+def GetCoinsByKey(key):
+  con = lite.connect("blockchain.db")
+  with con:
+    c = con.cursor()
+    c.execute("SELECT * FROM Blockchain WHERE PubKey = '{k}';".format(k=key))
+    row = c.fetchall()
+    if not row:
+      return None
+    return row[0][2] # return the amount of coins
+
 CreateDataBase()
 AddPubKey("91298e9812eh81298he9h182e", 420)
 PrintPubKeys()
+print(GetCoinsByKey("91298e9812eh81298he9h182e")) # => 420
+print(GetCoinsByKey("invalid_key"))               # => None
 
 DeleteDataBase()
